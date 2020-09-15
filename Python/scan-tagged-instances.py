bdcca5eec59149d8df167b6d4c9dd074ada4b2fe
Wagner, J <35028309+dota247@users.noreply.github.com>
2020-09-15 11:24:35 -0700

    Update scan-tagged-instances.py

--- Python/scan-tagged-instances.py
+++ Python/scan-tagged-instances.py
@@ -1,8 +1,18 @@
-
+#libraries
 import boto3
 
+#instance variables
+aws_region = "test"
+
+#code
 session = boto3.Session(profile_name='default')
 
 ec2 = boto3.client('ec2')
+
 response = ec2.describe_instances()
+
+#rint("Scanning region: {}".format(aws_region))
+
+for i in response
+
 print(response)
