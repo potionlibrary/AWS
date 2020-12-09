0d0d64833b36769cb3c7727415d73624b5de11f9
Wagner, J <35028309+dota247@users.noreply.github.com>
2020-12-09 12:56:42 -0800

    Create query-old-snapshots.py

---
+++ Python/query-old-snapshots.py
@@ -0,0 +1,25 @@
+# Use this to refrence calling objects.
+# Author - JW
+
+#libraries
+import boto3
+
+
+aws_regions = ['us-east-2']
+tags_to_find = ['Backup', 'backup']
+
+def lambda_handler(event, context):
+    
+    ec2 = boto3.client('ec2', region_name='us-east-2')
+    response = ec2.describe_instances(Filters=[{'Name' : 'tag-key','Values' : tags_to_find },]) 
+    
+    for i in response["Reservations"]:
+    
+        # if the TimeStarted matches the date, append to a list
+        
+        
+        print(i["Instances"][0]["PrivateDnsName"])
+        print(i["Instances"][0]["PrivateIpAddress"])
+        print(i["Instances"][0]["State"])
+        print(i["Instances"][0]["InstanceId"])
+        print(i["Instances"][0]["BlockDeviceMappings"])
